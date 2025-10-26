e047812eb5a19cfced974861d1aba4b7825fc96f
Rubeneennnn <rubeninigueztorron@gmail.com>
2025-10-26 13:19:02 +0100

    test js mal

--- js/test.js
+++ js/test.js
@@ -2,8 +2,19 @@ function Comprobar() {
     event.preventDefault()
     rdRespuestasWWW = document.getElementsByName("www")
     respuestaOk = false
+    txtCheck = document.getElementById("txtCheck")
     for (radio of rdRespuestasWWW) {
-        if (radio.checked)
-            
+        if (radio.checked) {
+            txtCheck.innerHTML = "Respuesta: " + radio.value
+            if (radio.value == "timcorrect") {
+                respuestaOk = true
+            }
+        }
+    }
+    if (respuestaOk) {
+        txtCheck.innerHTML += " ¡Correcto!"
+        document.getElementById("siguiente").innerHTML = '<a href="test2.html">Siguiente pregunta</a>'
+    } else {
+        txtCheck.innerHTML += " Incorrecto. Inténtalo de nuevo."
     }
 }
