30716a6905348a895879e0f4911858ae0c584125
Rubeneennnn <rubeninigueztorron@gmail.com>
2025-11-01 21:38:03 +0100

    acabar js y mejorar css

--- js/test.js
+++ js/test.js
@@ -1,43 +1,89 @@
 function Comprobar() {
     event.preventDefault()
-    rdRespuestasWWW = document.getElementsByName("www")
-    cbRespuestasHTML = document.getElementsByName("html")
-    stRespuestasWEB = document.getElementById("web")
-    dtRespuestasFECHA = document.getElementsByName("fecha")
-    cbRespuestasBASES = document.getElementsByName("bases")
-    respuestaOk = false
-    txtCheck = document.getElementById("txtCheck")
+    let rdRespuestasWWW = document.getElementsByName("www")
+    let cbRespuestasHTML = document.getElementsByName("html")
+    let stRespuestasWEB = document.getElementById("web")
+    let dtRespuestasFECHA = document.getElementsByName("fecha")
+    let cbRespuestasBASES = document.getElementsByName("bases")
+    let respuestaOk = false
+    let txtCheck = document.getElementById("txtCheck")
+    let puntuacion = 0;
+    let totalPreguntas = 5;
+    document.querySelectorAll(".iconoResultado").forEach(img => img.remove());
     for (radio of rdRespuestasWWW) {
         if (radio.checked) {
             txtCheck.innerHTML = "Respuesta: " + radio.value
-            if (radio.value == "Correcta") {
+            if (radio.value == "bernersLee") {
                 respuestaOk = true
             }
+        }
+        if (respuestaOk) {
+            puntuacion++;
+            txtCheck.innerHTML += " (Correcta)";
+            document.getElementById("Pregunta_4").innerHTML += '<img class="iconoResultado" src="../media/correcta.png" alt="Correcto">';
+        } else {
+            txtCheck.innerHTML += " (Incorrecta)";
+            document.getElementById("Pregunta_4").innerHTML += '<img class="iconoResultado" src="../media/incorrecta.png" alt="Incorrecto">';
         }
     }
     for (checkbox of cbRespuestasHTML) {
         if (checkbox.checked) {
             txtCheck.innerHTML = "Respuesta: " + checkbox.value
-            if (checkbox.value == "Correcta") {
+            if (checkbox.value == "enlaces, tablas, videos") {
                 respuestaOk = true
             }
         }
+        if (respuestaOk) {
+            puntuacion++;
+            txtCheck.innerHTML += " (Correcta)";
+            document.getElementById("Pregunta_5").innerHTML += '<img class="iconoResultado" src="../media/correcta.png" alt="Correcto">';
+        } else {
+            txtCheck.innerHTML += " (Incorrecta)";
+            document.getElementById("Pregunta_5").innerHTML += '<img class="iconoResultado" src="../media/incorrecta.png" alt="Incorrecto">';
+        }
     }
     for (select of stRespuestasWEB) {
-        if (select.value == "Correcta") {
+        if (select.value == "cero") {
             respuestaOk = true
         }
         txtCheck.innerHTML = "Respuesta: " + select.value
+        if (respuestaOk) {
+            puntuacion++;
+            txtCheck.innerHTML += " (Correcta)";
+            document.getElementById("Pregunta_6").innerHTML += '<img class="iconoResultado" src="../media/correcta.png" alt="Correcto">';
+        } else {
+            txtCheck.innerHTML += " (Incorrecta)";
+            document.getElementById("Pregunta_6").innerHTML += '<img class="iconoResultado" src="../media/incorrecta.png" alt="Incorrecto">';
+        }
     }
     for (date of dtRespuestasFECHA) {
         txtCheck.innerHTML = "Respuesta: " + date.value
+        if (date.value == "1991-03-12") {
+            respuestaOk = true
+        }
+        if (respuestaOk) {
+            puntuacion++;
+            txtCheck.innerHTML += " (Correcta)";
+            document.getElementById("Pregunta_7").innerHTML += '<img class="iconoResultado" src="../media/correcta.png" alt="Correcto">';
+        } else {
+            txtCheck.innerHTML += " (Incorrecta)";
+            document.getElementById("Pregunta_7").innerHTML += '<img class="iconoResultado" src="../media/incorrecta.png" alt="Incorrecto">';
+        }
     }
     for (checkbox of cbRespuestasBASES) {
         if (checkbox.checked) {
             txtCheck.innerHTML = "Respuesta: " + checkbox.value
-            if (checkbox.value == "Correcta") {
+            if (checkbox.value == "json, xml") {
                 respuestaOk = true
             }
+        }
+        if (respuestaOk) {
+            puntuacion++;
+            txtCheck.innerHTML += " (Correcta)";
+            document.getElementById("Pregunta_8").innerHTML += '<img class="iconoResultado" src="../media/correcta.png" alt="Correcto">';
+        } else {
+            txtCheck.innerHTML += " (Incorrecta)";
+            document.getElementById("Pregunta_8").innerHTML += '<img class="iconoResultado" src="../media/incorrecta.png" alt="Incorrecto">';
         }
     }
 }
