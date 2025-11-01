578e1a116ae9483e5fa2382fe9601c00b9ddb259
Rubeneennnn <rubeninigueztorron@gmail.com>
2025-11-01 17:40:37 +0100

    mejorar el test js

--- js/test.js
+++ js/test.js
@@ -1,12 +1,41 @@
 function Comprobar() {
     event.preventDefault()
     rdRespuestasWWW = document.getElementsByName("www")
+    cbRespuestasHTML = document.getElementsByName("html")
+    stRespuestasWEB = document.getElementById("web")
+    dtRespuestasFECHA = document.getElementsByName("fecha")
+    cbRespuestasBASES = document.getElementsByName("bases")
     respuestaOk = false
     txtCheck = document.getElementById("txtCheck")
     for (radio of rdRespuestasWWW) {
         if (radio.checked) {
             txtCheck.innerHTML = "Respuesta: " + radio.value
-            if (radio.value == "timcorrect") {
+            if (radio.value == "Correcta") {
+                respuestaOk = true
+            }
+        }
+    }
+    for (checkbox of cbRespuestasHTML) {
+        if (checkbox.checked) {
+            txtCheck.innerHTML = "Respuesta: " + checkbox.value
+            if (checkbox.value == "Correcta") {
+                respuestaOk = true
+            }
+        }
+    }
+    for (select of stRespuestasWEB) {
+        if (select.value == "Correcta") {
+            respuestaOk = true
+        }
+        txtCheck.innerHTML = "Respuesta: " + select.value
+    }
+    for (date of dtRespuestasFECHA) {
+        txtCheck.innerHTML = "Respuesta: " + date.value
+    }
+    for (checkbox of cbRespuestasBASES) {
+        if (checkbox.checked) {
+            txtCheck.innerHTML = "Respuesta: " + checkbox.value
+            if (checkbox.value == "Correcta") {
                 respuestaOk = true
             }
         }
