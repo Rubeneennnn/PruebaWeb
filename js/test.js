68514d066965d7d174484823ae41e4b6d1e0703e
Rubeneennnn <rubeninigueztorron@gmail.com>
2025-11-02 18:23:14 +0100

    arreglar test js

--- js/test.js
+++ js/test.js
@@ -6,84 +6,72 @@ function Comprobar() {
     let dtRespuestasFECHA = document.getElementsByName("fecha")
     let cbRespuestasBASES = document.getElementsByName("bases")
     let respuestaOk = false
-    let txtCheck = document.getElementById("txtCheck")
     let puntuacion = 0;
-    let totalPreguntas = 5;
     document.querySelectorAll(".iconoResultado").forEach(img => img.remove());
     for (radio of rdRespuestasWWW) {
         if (radio.checked) {
-            txtCheck.innerHTML = "Respuesta: " + radio.value
+            puntuacion.innerHTML = "Respuesta: " + radio.value
             if (radio.value == "bernersLee") {
                 respuestaOk = true
             }
         }
-        if (respuestaOk) {
-            puntuacion++;
-            txtCheck.innerHTML += " (Correcta)";
-            document.getElementById("Pregunta_4").innerHTML += '<img class="iconoResultado" src="../media/correcta.png" alt="Correcto">';
+        if (respuestaOk = true) {
+            puntuacion += 1;
+            document.getElementById("Pregunta_4").src="../media/correcta.png";
         } else {
-            txtCheck.innerHTML += " (Incorrecta)";
-            document.getElementById("Pregunta_4").innerHTML += '<img class="iconoResultado" src="../media/incorrecta.png" alt="Incorrecto">';
+            document.getElementById("Pregunta_4").src="../media/incorrecta.png";
         }
     }
     for (checkbox of cbRespuestasHTML) {
         if (checkbox.checked) {
-            txtCheck.innerHTML = "Respuesta: " + checkbox.value
+            puntuacion.innerHTML = "Respuesta: " + checkbox.value
             if (checkbox.value == "enlaces, tablas, videos") {
                 respuestaOk = true
             }
         }
-        if (respuestaOk) {
-            puntuacion++;
-            txtCheck.innerHTML += " (Correcta)";
-            document.getElementById("Pregunta_5").innerHTML += '<img class="iconoResultado" src="../media/correcta.png" alt="Correcto">';
+        if (respuestaOk = true) {
+            puntuacion += 1;
+            document.getElementById("Pregunta_5").src="../media/correcta.png";
         } else {
-            txtCheck.innerHTML += " (Incorrecta)";
-            document.getElementById("Pregunta_5").innerHTML += '<img class="iconoResultado" src="../media/incorrecta.png" alt="Incorrecto">';
+            document.getElementById("Pregunta_5").src="../media/incorrecta.png";
         }
     }
     for (select of stRespuestasWEB) {
         if (select.value == "cero") {
             respuestaOk = true
         }
-        txtCheck.innerHTML = "Respuesta: " + select.value
-        if (respuestaOk) {
-            puntuacion++;
-            txtCheck.innerHTML += " (Correcta)";
-            document.getElementById("Pregunta_6").innerHTML += '<img class="iconoResultado" src="../media/correcta.png" alt="Correcto">';
+        puntuacion.innerHTML = "Respuesta: " + select.value
+        if (respuestaOk = true) {
+            puntuacion += 1;
+            document.getElementById("Pregunta_6").src="../media/correcta.png";
         } else {
-            txtCheck.innerHTML += " (Incorrecta)";
-            document.getElementById("Pregunta_6").innerHTML += '<img class="iconoResultado" src="../media/incorrecta.png" alt="Incorrecto">';
+            document.getElementById("Pregunta_6").src="../media/incorrecta.png";
         }
     }
     for (date of dtRespuestasFECHA) {
-        txtCheck.innerHTML = "Respuesta: " + date.value
+        puntuacion.innerHTML = "Respuesta: " + date.value
         if (date.value == "1991-03-12") {
             respuestaOk = true
         }
-        if (respuestaOk) {
-            puntuacion++;
-            txtCheck.innerHTML += " (Correcta)";
-            document.getElementById("Pregunta_7").innerHTML += '<img class="iconoResultado" src="../media/correcta.png" alt="Correcto">';
+        if (respuestaOk = true) {
+            puntuacion += 1;
+            document.getElementById("Pregunta_7").src="../media/correcta.png";
         } else {
-            txtCheck.innerHTML += " (Incorrecta)";
-            document.getElementById("Pregunta_7").innerHTML += '<img class="iconoResultado" src="../media/incorrecta.png" alt="Incorrecto">';
+            document.getElementById("Pregunta_7").src="../media/incorrecta.png";
         }
     }
     for (checkbox of cbRespuestasBASES) {
         if (checkbox.checked) {
-            txtCheck.innerHTML = "Respuesta: " + checkbox.value
+            puntuacion.innerHTML = "Respuesta: " + checkbox.value
             if (checkbox.value == "json, xml") {
                 respuestaOk = true
             }
         }
-        if (respuestaOk) {
-            puntuacion++;
-            txtCheck.innerHTML += " (Correcta)";
-            document.getElementById("Pregunta_8").innerHTML += '<img class="iconoResultado" src="../media/correcta.png" alt="Correcto">';
+        if (respuestaOk = true) {
+            puntuacion += 1;
+            document.getElementById("Pregunta_8").src="../media/correcta.png";
         } else {
-            txtCheck.innerHTML += " (Incorrecta)";
-            document.getElementById("Pregunta_8").innerHTML += '<img class="iconoResultado" src="../media/incorrecta.png" alt="Incorrecto">';
+            document.getElementById("Pregunta_8").src="../media/incorrecta.png";
         }
     }
 }
