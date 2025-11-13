40344bb64807fb9b9084f178be7d9af686325b7d
Rubeneennnn <rubeninigueztorron@gmail.com>
2025-11-13 08:06:04 +0100

    a

--- js/test.js
+++ js/test.js
@@ -16,11 +16,13 @@ function Comprobar() {
             respuestawww = true;
         }
     }
-    if (respuestawww == true) {
-        puntuacion++;
-        document.getElementById("imgP4").src = "../media/correcta.png";
+    const imgP4 = document.getElementById("imgP4");
+    if ([...rdRespuestasWWW].some(r => r.checked)) {
+        imgP4.style.display = "inline";
+        imgP4.src = respuestawww ? "../media/correcta.png" : "../media/incorrecta.png";
+        if (respuestawww) puntuacion++;
     } else {
-        document.getElementById("imgP4").src = "../media/incorrecta.png";
+        imgP4.style.display = "none";
     }
     let htmlCorrectas = ["enlaces", "tablas", "videos"];
     let htmlSeleccionadas = [];
@@ -35,31 +37,37 @@ function Comprobar() {
     ) {
         respuestahtml = true;
     }
-    if (respuestahtml == true) {
-        puntuacion++;
-        document.getElementById("imgP5").src = "../media/correcta.png";
+    const imgP5 = document.getElementById("imgP5");
+    if ([...cbRespuestasHTML].some(c => c.checked)) {
+        imgP5.style.display = "inline";
+        imgP5.src = respuestahtml ? "../media/correcta.png" : "../media/incorrecta.png";
+        if (respuestahtml) puntuacion++;
     } else {
-        document.getElementById("imgP5").src = "../media/incorrecta.png";
+        imgP5.style.display = "none";
     }
     if (stRespuestasWEB.value == "cero") {
         respuestaweb = true;
     }
-    if (respuestaweb == true) {
-        puntuacion++;
-        document.getElementById("imgP6").src = "../media/correcta.png";
+    const imgP6 = document.getElementById("imgP6");
+    if (stRespuestasWEB.value !== "") {
+        imgP6.style.display = "inline";
+        imgP6.src = respuestaweb ? "../media/correcta.png" : "../media/incorrecta.png";
+        if (respuestaweb) puntuacion++;
     } else {
-        document.getElementById("imgP6").src = "../media/incorrecta.png";
+        imgP6.style.display = "none";
     }
     for (date of dtRespuestasFECHA) {
         if (date.value == "1969-01-01") {
             respuestafecha = true;
         }
     }
-    if (respuestafecha == true) {
-        puntuacion++;
-        document.getElementById("imgP7").src = "../media/correcta.png";
+    const imgP7 = document.getElementById("imgP7");
+    if ([...dtRespuestasFECHA].some(d => d.value)) {
+        imgP7.style.display = "inline";
+        imgP7.src = respuestafecha ? "../media/correcta.png" : "../media/incorrecta.png";
+        if (respuestafecha) puntuacion++;
     } else {
-        document.getElementById("imgP7").src = "../media/incorrecta.png";
+        imgP7.style.display = "none";
     }
     let basesCorrectas = ["json", "xml"];
     let basesSeleccionadas = [];
@@ -74,15 +82,21 @@ function Comprobar() {
     ) {
         respuestabases = true;
     }
-    if (respuestabases == true) {
-        puntuacion++;
-        document.getElementById("imgP8").src = "../media/correcta.png";
+    const imgP8 = document.getElementById("imgP8");
+    if ([...cbRespuestasBASES].some(c => c.checked)) {
+        imgP8.style.display = "inline";
+        imgP8.src = respuestabases ? "../media/correcta.png" : "../media/incorrecta.png";
+        if (respuestabases) puntuacion++;
     } else {
-        document.getElementById("imgP8").src = "../media/incorrecta.png";
+        imgP8.style.display = "none";
     }
     document.getElementById("puntuacionTotal").textContent = puntuacion;
+    const imgF = document.getElementById("imgF");
     if (puntuacion == 5) {
-        document.getElementById("imgF").src = "../media/final.jpg";
+        imgF.src = "../media/final.png";
+        imgF.style.display = "inline";
+    } else {
+        imgF.style.display = "none";
     }
 }
 
@@ -97,4 +111,11 @@ function Limpiar() {
         if (img) img.src = "";
     });
     document.getElementById("puntuacionTotal").textContent = "";
+    ["imgP4", "imgP5", "imgP6", "imgP7", "imgP8", "imgF"].forEach(id => {
+    const img = document.getElementById(id);
+    if (img) {
+        img.src = "";
+        img.style.display = "none";
+    }
+    });
 }
